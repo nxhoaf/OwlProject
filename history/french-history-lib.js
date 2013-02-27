63e0ee02c76023c1088c60ab9bba6a8f706caf1d
nxhoaf <nxhoaf@gmail.com>
2013-02-27 11:29:14 +0100

    fixed namespace in french-history.js

--- history/french-history-lib.js
+++ history/french-history-lib.js
@@ -1,56 +1,12 @@
 FrHistoryLib = {};
 FrHistoryLib.constant = {};
 
-//************ INITIALIZATION: create constant based on browser info***********
-/**
- * Get browser info
- * http://stackoverflow.com/questions/5916900/detect-version-of-browser
- */
-FrHistoryLib.getBrowserInfo = function() {
-	var N = navigator.appName, ua = navigator.userAgent, tem;
-	var M = ua
-			.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
-	if (M && (tem = ua.match(/version\/([\.\d]+)/i)) != null)
-		M[2] = tem[1];
-	M = M ? [ M[1], M[2] ] : [ N, navigator.appVersion, '-?' ];
-	
-	return M;
-};
 
-
-//For now, our library works only with chrome and firefox
-FrHistoryLib.constant.firefox = {
+FrHistoryLib.constant = {
 	FAIT_PARTIE_DE 	: "faitPartieDe",
 	ORGANIZATION 	: "organisation-systeme-scolaire-francais",
 	PROGRAM 		: "programme_histoire_college_france"	
 }
-
-FrHistoryLib.constant.chrome = {
-	FAIT_PARTIE_DE 	: "faitPartieDe",
-	ORGANIZATION 	: "organisation-systeme-scolaire-francais",
-	PROGRAM 		: "programme_histoire_college_france"
-}
-
-var browserInfo = FrHistoryLib.getBrowserInfo();
-var browserName = browserInfo[0];
-browserName = browserName.toLowerCase();
-var browserVersion = browserInfo[0];
-browserVersion = browserVersion.toLowerCase();
-// Config constant based on browser type
-if ("firefox" === browserName) {
-	FrHistoryLib.constant = {
-		FAIT_PARTIE_DE 	: FrHistoryLib.constant.firefox.FAIT_PARTIE_DE, 
-		ORGANIZATION 	: FrHistoryLib.constant.firefox.ORGANIZATION, 
-		PROGRAM 		: FrHistoryLib.constant.firefox.PROGRAM 
-	}
-} else { // chrome
-	FrHistoryLib.constant = {
-		FAIT_PARTIE_DE 	: FrHistoryLib.constant.chrome.FAIT_PARTIE_DE, 
-		ORGANIZATION 	: FrHistoryLib.constant.chrome.ORGANIZATION, 
-		PROGRAM 		: FrHistoryLib.constant.chrome.PROGRAM 
-	}
-}
-//********************** END OF INITIALIZATION *********************************
 
 FrHistoryLib.getFaitPartieDe = function (element) {
 	var properties = {};
@@ -119,13 +75,5 @@ FrHistoryLib.getKnowledgeOf = function (theme) {
 		result.push(stProperties);
 	}
 	
-	for (var i = 0; i < result.length; i++) {
-		var subTheme = result[i];
-		console.log(i + "  ------------------------------")
-		for (var key in subTheme) {
-			console.log("	key:   " + key);
-			console.log("	value: " + subTheme[key]);
-		}
-	}
 	return result;
 }
